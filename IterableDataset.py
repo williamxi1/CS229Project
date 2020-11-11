8d707d30fe6d48d7b4da2e3cd08ec0cceff8a297
ericbfeng <58924684+ericbfeng@users.noreply.github.com>
2020-11-11 00:03:58 -0500

    helloOOOOOOSosAdoasdoasodOAOOOO

--- IterableDataset.py
+++ IterableDataset.py
@@ -45,10 +45,48 @@ class FashionDataset(Dataset):
         return sample
 
 
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, landmarks = sample['image'], sample['landmarks']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        img = transform.resize(image, (new_h, new_w))
+
+        # h and w are swapped for landmarks because for images,
+        # x and y axes are axis 1 and 0 respectively
+        landmarks = landmarks * [new_w / w, new_h / h]
+
+        return {'image': img, 'landmarks': landmarks}
+
+
+
+
+
+
 shoe_dataset = FashionDataset(csv_file='../data/shoes.csv',
                                     root_dir='../data/Shoes/')
-
-
 
 for i in range(len(shoe_dataset)):
     sample = shoe_dataset[i]
