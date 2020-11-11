dd62716438a5a3cbb74005601c45c600f122f1bc
williamxi1 <wxi@stanford.edu>
2020-11-10 23:26:10 -0500

    asdf

--- IterableDataset.py
+++ IterableDataset.py
@@ -36,9 +36,29 @@ class FashionDataset(Dataset):
                                 self.fashion_frame.iloc[idx, 0])
         image = io.imread(img_name)
         catagory = self.fashion_frame.iloc[idx, 1]
-        sample = {'image': image, 'landmarks': catagory}
+        sample = {'image': image, 'category': catagory}
 
         if self.transform:
             sample = self.transform(sample)
 
         return sample
+
+shoe_dataset = FashionDataset(csv_file='data/faces/face_landmarks.csv',
+                                    root_dir='data/faces/')
+
+fig = plt.figure()
+
+for i in range(len(shoe_dataset)):
+    sample = shoe_dataset[i]
+
+    print(i, sample['image'].shape, sample['category'].shape)
+
+    ax = plt.subplot(1, 4, i + 1)
+    plt.tight_layout()
+    ax.set_title('Sample #{}'.format(i))
+    ax.axis('off')
+    show_landmarks(**sample)
+
+    if i == 3:
+        plt.show()
+        break
