b6bd8d9887f7b941d312941313c62a4f8814f773
williamxi1 <wxi@stanford.edu>
2020-11-12 22:39:40 -0500

    asdf

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ dataloader = torch.utils.data.DataLoader(
         "../data/fashion-dataset/shoes.csv",
         "../data/fashion-dataset/Shoes",
         transform=transforms.Compose(
-            [Rescale(64), ToTensor()]
+            [Rescale((64,64)), ToTensor()]
         ),
     ),
     batch_size=opt.batch_size,
