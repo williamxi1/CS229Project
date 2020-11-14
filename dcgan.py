5d8bd8f9503e3a3d0ef5b24bda2999e1e4254004
williamxi1 <wxi@stanford.edu>
2020-11-14 00:42:35 -0500

    asdf

--- dcgan.py
+++ dcgan.py
@@ -29,7 +29,7 @@ torch.manual_seed(manualSeed)
 workers = 2 #Number of workers for dataloader
 batch_size = 64 #Batch size during training
 image_size = 64 #Spatial size of training images. All images will be resized to this size using a transformer.
-nc = 3 # Number of channels in the training images. For color images this is 3
+nc = 1 # Number of channels in the training images. For color images this is 3
 nz = 100 # Size of z latent vector (i.e. size of generator input)
 ngf = 64 # Size of feature maps in generator
 ndf = 64 # Size of feature maps in discriminator
